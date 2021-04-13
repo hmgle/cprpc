acfefe35fec328eb43f0dffd8a17c26cc1a21b61
mingang.he <dustgle@gmail.com>
2021-04-13 21:42:31 +0800

    Add msgpackrpc

---
+++ codec/msgpackrpc/client.go
@@ -0,0 +1,50 @@
+package msgpackrpc
+
+import (
+	"bufio"
+	"io"
+
+	"github.com/hashicorp/go-msgpack/codec"
+	"github.com/hmgle/cprpc"
+)
+
+type clientCodec struct {
+	rwc    io.ReadWriteCloser
+	dec    *codec.Decoder // for reading msgpack values
+	enc    *codec.Encoder // for writing msgpack values
+	encBuf *bufio.Writer
+}
+
+var _ cprpc.ClientCodec = &clientCodec{}
+
+// NewClientCodec returns a new cprpc.ClientCodec using msgpack-rpc on conn.
+func NewClientCodec(conn io.ReadWriteCloser) cprpc.ClientCodec {
+	return &clientCodec{
+		dec:    codec.NewDecoder(conn, msgpackHandle),
+		enc:    codec.NewEncoder(conn, msgpackHandle),
+		rwc:    conn,
+		encBuf: bufio.NewWriter(conn),
+	}
+}
+
+func (c *clientCodec) WriteRequest(r *cprpc.Request, body interface{}) (err error) {
+	if err = c.enc.Encode(r); err != nil {
+		return
+	}
+	if err = c.enc.Encode(body); err != nil {
+		return
+	}
+	return c.encBuf.Flush()
+}
+
+func (c *clientCodec) ReadResponseHeader(r *cprpc.Response) error {
+	return c.dec.Decode(r)
+}
+
+func (c *clientCodec) ReadResponseBody(body interface{}) error {
+	return c.dec.Decode(body)
+}
+
+func (c *clientCodec) Close() error {
+	return c.rwc.Close()
+}
